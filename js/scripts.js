b24b0fc89b907afce2136b5c4d8ea7d0d9568f66
Athyuttam Reddy <athyuttamre@gmail.com>
2014-04-06 21:45:59 -0400

    Added final registration section

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
-	$("#registration-form").submit(function(e) {
+	$("#registration-form, #final-registration-form").submit(function(e) {
 		e.preventDefault();
-		var email = $("#hero-email").val();
+		var email = ($(this).attr("id") == "registration-form") ? $("#hero-email").val() : $("#final-hero-email").val();
 
 		if (email == "") {
-			$("#registration-form").addClass("animated shake");
-			setTimeout(function() {$('#registration-form').removeClass("animated shake")}, 850);
+			$(this).addClass("animated shake");
+			setTimeout(function() {$(this).removeClass("animated shake")}, 850);
 			return;
 		} else {
 			var registrationFormURL = "https://docs.google.com/forms/d/14ivINPXtLc1II63VhjRUACnzxwNu41LmKhYCE94yBJ4/viewform?entry.444215346&entry.1007700452&entry.1440565968="
@@ -20,15 +20,15 @@ $(document).ready(function() {
     $(window).scroll( function(){
     
         /* Check the location of each desired element */
-        $('.hideme').each( function(i){
+        $('.fading-content').each( function(i){
             
-            var third_of_object = $(this).position().top + ($(this).outerHeight() / 3);
+            var quarter_of_object = $(this).position().top + ($(this).outerHeight() / 4);
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > third_of_object){
+            if( bottom_of_window > quarter_of_object){
                 
-                $(this).animate({'opacity':'1'},500);
+                $(this).animate({'opacity':'1'},300);
                     
             }
             
